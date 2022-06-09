f34799d684a6a3a19334ca55b280ead5e0f4d7f8
fadildillah <fadillahm704@gmail.com>
2022-06-09 19:29:58 +0700

    minor fixes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const Footer = () => {
   return (
-    <div className='flex justify-center items-center h-16 bg-grey-400 shadow-lg sticky bottom-0'>
+    <div className='flex justify-center items-center h-16 bg-grey-400 shadow-lg'>
         <p className='text-center text-bold text-black text-xl'>
             Sites create by <a href='https://linkedin.com/in/muhammad-fadillah-491668192/' className='text-blue-500'>Fadil ✌️</a><br/>
             Using <a href='https://swapi.dev/' className='text-blue-500'>SWAPI API</a>
